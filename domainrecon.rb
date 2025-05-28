798724eb0559b23587dd7979c2a30f43e66ff401
gustavfroding <49125732+gustavfroding@users.noreply.github.com>
2025-05-28 09:42:12 +0200

    0.2.2

--- domainrecon.rb
+++ domainrecon.rb
@@ -3,8 +3,8 @@ class Domainrecon < Formula
   
     desc "domainrecon is a CLI tool for mapping the technology stack of a domain using WHOIS, DNS, certificate transparency logs, and HTTPS analysis."
     homepage "https://github.com/gustavfroding/domainrecon"
-    url "https://github.com/gustavfroding/domainrecon/archive/refs/tags/v0.2.1.tar.gz"
-    sha256 "88d5fa2bdf832fff45785e1e67403b42c7878b3de166553bfde13f1f94b3ab50"
+    url "https://github.com/gustavfroding/domainrecon/archive/refs/tags/v0.2.2.tar.gz"
+    sha256 "607f4752f8648068da52a4c1ca089a266bc8f859bba5c7f6cffc05367241e61a"
     license "MIT"
   
     depends_on "python@3.11"
