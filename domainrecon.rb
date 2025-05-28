e1803279d10f6759729b085602ee88a288ddbaef
gustavfroding <49125732+gustavfroding@users.noreply.github.com>
2025-05-28 09:00:33 +0200

    typo

--- domainrecon.rb
+++ domainrecon.rb
@@ -1,7 +1,7 @@
-class Crtlookup < Formula
+class Domainrecon < Formula
     include Language::Python::Virtualenv
   
-    desc "domainstack is a CLI tool for mapping the technology stack of a domain using WHOIS, DNS, certificate transparency logs, and HTTPS analysis."
+    desc "domainrecon is a CLI tool for mapping the technology stack of a domain using WHOIS, DNS, certificate transparency logs, and HTTPS analysis."
     homepage "https://github.com/gustavfroding/domainrecon"
     url "https://github.com/gustavfroding/domainrecon/archive/refs/tags/v0.1.0.tar.gz"
     sha256 "7a0433d63597a60de1ead234580efb70614184c6b168491342470111a66d579e"
