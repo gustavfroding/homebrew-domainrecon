fb14e88fc64cbd3e1c07a0f21f6cafeb15142e5b
gustavfroding <49125732+gustavfroding@users.noreply.github.com>
2025-06-04 07:13:22 +0200

    0.2.3

--- domainrecon.rb
+++ domainrecon.rb
@@ -1,15 +1,45 @@
 class Domainrecon < Formula
-    include Language::Python::Virtualenv
-  
-    desc "domainrecon is a CLI tool for mapping the technology stack of a domain using WHOIS, DNS, certificate transparency logs, and HTTPS analysis."
-    homepage "https://github.com/gustavfroding/domainrecon"
-    url "https://github.com/gustavfroding/domainrecon/archive/refs/tags/v0.2.2.tar.gz"
-    sha256 "607f4752f8648068da52a4c1ca089a266bc8f859bba5c7f6cffc05367241e61a"
-    license "MIT"
-  
-    depends_on "python@3.11"
-  
-    def install
-      virtualenv_install_with_resources
-    end
+  include Language::Python::Virtualenv
+
+  desc "CLI tool for mapping technology stack of a domain"
+  homepage "https://github.com/gustavfroding/domainrecon"
+  url "https://github.com/gustavfroding/domainrecon/archive/refs/tags/v0.2.3.tar.gz"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  license "MIT"
+
+  depends_on "python@3.11"
+
+  resource "rich" do
+    url "https://files.pythonhosted.org/packages/a1/53/830aa4c3066a8ab0ae9a9955976fb770fe9c6102117c8ec4ab3ea62d89e8/rich-14.0.0.tar.gz"
+    sha256 "1c9491e1951aac09caffd42f448ee3d04e58923ffe14993f6e83068dc395d7e0"
   end
+
+  resource "python-whois" do
+    url "https://files.pythonhosted.org/packages/f5/5b/5c0c705d14945954b23b69042c1597971da3cd6dc3ed23b96449be91d665/python_whois-0.9.5.tar.gz"
+    sha256 "d435cf8012659745ce3b4e7c15428df9405648696727812d9eaacc36782d14e3"
+  end
+
+  resource "dnspython" do
+    url "https://files.pythonhosted.org/packages/b5/4a/263763cb2ba3816dd94b08ad3a33d5fdae34ecb856678773cc40a3605829/dnspython-2.7.0.tar.gz"
+    sha256 "b4c34b7d10b51bcc3a5071e7b8dee77939f1e878477eeecc965e9835f63c6c86"
+  end
+
+  resource "ipwhois" do
+    url "https://files.pythonhosted.org/packages/ed/e9/f1dd533366b5809494d0ee0d39db2e0806388c52c25b1dcff2e0794a51d8/ipwhois-1.3.0.tar.gz"
+    sha256 "ad2cd7e7b8c81044fc8829bb4357bce6088c654a20f51f5d234fe6a99e0fbf90"
+  end
+
+  resource "aiohttp" do
+    url "https://files.pythonhosted.org/packages/eb/62/95588e933dfea06a3af0332990bd19f6768f8f37fa4c0fe33fe4c55cf9d0/aiohttp-3.12.7.tar.gz"
+    sha256 "6f04af3bf040dc8fd9b2bc0e465f5aca6fc5349fa08bd7f08142974a2ded21bf"
+  end
+
+  resource "bs4" do
+    url "https://files.pythonhosted.org/packages/c9/aa/4acaf814ff901145da37332e05bb510452ebed97bc9602695059dd46ef39/bs4-0.0.2.tar.gz"
+    sha256 "abf8742c0805ef7f662dce4b51cca104cffe52b835238afc169142ab9b3fbccc"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+end
